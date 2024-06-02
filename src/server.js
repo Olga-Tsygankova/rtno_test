905d938682c49ddc2b90831ac38cf9c3d5e49882
Olga Tsygankova <tsygankova@perx.ru>
2024-06-03 00:06:01 +0300

    попытка 2

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3002');
+    res.setHeader('Access-Control-Allow-Origin', 'https://rtno-test-gamma.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
