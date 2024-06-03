45ee22b264296170cb15964207f392b6eed66de3
Olga Tsygankova <tsygankova@perx.ru>
2024-06-03 18:24:32 +0300

    Добавила side-bar + icons

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,7 +1,26 @@
-export const SideBar=()=>{
-    return(
+import {Attach, Best, Calendar, Call, EveryWhere, Quickly} from "../../icons/Icons";
+
+export const SideBar = () => {
+    return (
         <div>
-            Боковая панель
+            <a href='#'>
+                <Attach/>
+            </a>
+            <a href='#'>
+                <Best/>
+            </a>
+            <a href='#'>
+                <Calendar/>
+            </a>
+            <a href='#'>
+                <Call/>
+            </a>
+            <a href='#'>
+                <EveryWhere/>
+            </a>
+            <a href='#'>
+                <Quickly/>
+            </a>
         </div>
     )
 }
