ae943272f6564031910f44d44267c97e8bdc3966
Olga Tsygankova <tsygankova@perx.ru>
2024-06-05 05:48:46 +0300

    исправления 6

--- client/src/components/DialogsTable/DialogsTable.tsx
+++ client/src/components/DialogsTable/DialogsTable.tsx
@@ -26,7 +26,7 @@ export const DialogsTable = () => {
     const [employees, setEmployees] = useState<string[]>([]);
 
     useEffect(() => {
-        fetch('https://rtno-test-eight.vercel.app/')
+        fetch('https://rtno-test-eight.vercel.app/dialogs_with_comments')
             .then(response => response.json())
             .then(data => {
                 setDialogs(data);
